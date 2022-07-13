f4133a6a5890d3eef44726423acc6c253c645aea
Alexandre Junior <alexandrejunior408@gmail.com>
2022-07-12 21:38:24 -0300

    first commit

---
+++ introducao-ao-html/exercicio/index.js
@@ -0,0 +1,42 @@
+document.getElementById('app').innerHTML += `
+
+<div class='bgHello'>
+<h1>Hello World</h1>
+</div>
+
+<div class='bgHello'>
+<h2>Hello World</h2>
+</div>
+
+<div class='bgHello'>
+<h3>Hello World</h3>
+</div>
+
+<div class='bgHello'>
+<h4>Hello World</h4>
+</div>
+
+<div class='bgHello'>
+<h5>Hello World</h5>
+</div>
+
+<div class='bgHello'>
+<h6>Hello World</h6>
+</div>
+
+<div class='bgHello'>
+<p>Hello World</p>
+</div>
+
+<div class='bgHello'>
+<strong>Hello World</strong>
+</div>
+
+<div class='bgHello'>
+<li>Hello World</li>
+</div>
+
+<div class='bgHello'>
+<ol>Hello World</ol>
+</div>
+`;
